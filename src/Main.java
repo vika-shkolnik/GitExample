7baadfc9aada18738734dd699856aa3a277d924b
Victoria Vanyushkina <van_vika96@mail.ru>
2024-02-13 15:28:12 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Date());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        System.out.println(formatter.format(new Date()));
     }
 }
